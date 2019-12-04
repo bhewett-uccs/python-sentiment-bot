0eb4b12bdd1caef71ce4851070782be2d3b33b4a
Simeon <simeonwuthier@gmail.com>
2019-12-04 10:49:43 -0700

    Update Google Trends.py

--- Crawlers/Google Trends.py
+++ Crawlers/Google Trends.py
@@ -1,9 +1,11 @@
 import pytrends
+import pandas
 
 # Returns the sentiment for this service
 def fetch(keyword):
 	pytrend = TrendReq(hl='en-US', tz=360)
-
+	pytrends.build_payload([keyword], cat=0, timeframe='today 7-d', geo='', gprop='')
+	print(pytrends.interest_over_time())
 	return 1
 
 print('Initialized Google Trends')
