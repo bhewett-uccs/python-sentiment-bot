a7497dc3574565c6c565c9ddb465c205d0b8d607
Simeon <simeonwuthier@gmail.com>
2019-12-10 03:35:53 -0700

    USER INTERFACE ADDED

--- Main.py
+++ Main.py
@@ -1,11 +1,50 @@
+from __future__ import print_function # Replacing the print function to send into the user interface
+try:
+	import __builtin__
+except ImportError: # Python 3
+	import builtins as __builtin__
 import os
 import sys
 import traceback
 import re
+from PySide2 import QtGui
+from PySide2.QtCore import QFile
+from PySide2.QtCore import Slot
+from PySide2.QtGui import *
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import *
+import ctypes	# Used to make message boxes
+
+
+# Initialize the user interface window
+def initializeUserInterface():
+	global app
+	global window
+	global outputTxt
+	global computeBtn
+	global subjectTxt
+	global progressBar
+	global calendar
+
+	app = QApplication(sys.argv)
+	uiFile = QFile('Resources/interface.ui')
+	uiFile.open(QFile.ReadOnly)
+	loader = QUiLoader()
+	window = loader.load(uiFile)
+	uiFile.close()
+
+	outputTxt = window.findChild(QPlainTextEdit, 'outputTxt')
+	computeBtn = window.findChild(QPushButton, 'computeBtn')
+	subjectTxt = window.findChild(QLineEdit, 'subjectTxt')
+	progressBar = window.findChild(QProgressBar, 'progressBar')
+	calendar = window.findChild(QCalendarWidget, 'calendar')
+	computeBtn.clicked.connect(computeBtnClick)
+	window.show()
+
+	sys.exit(app.exec_())
 
 # Print a right-aligned message
 def rprint(text):
-
 	lines = text.split('\n')
 	if len(lines) == 1:
 		print(text.rjust(width))
@@ -27,11 +66,8 @@ def printException(e):
 def init():
 	global width
 	global crawlers
-	width = 80
 	crawlers = {}
 
-	os.system('cls')
-	print('_' * width)
 	rprint('Initializing modules...')
 
 	# Loop through each file in the Crawlers directory
@@ -59,6 +95,8 @@ def init():
 def getSentiment(keyword):
 	sumSentiments = 0
 	numSentiments = 0
+	count = 0
+	countMax = len(crawlers.items())
 	for (name, module) in crawlers.items():
 		print()
 		print('_' * width)
@@ -75,20 +113,56 @@ def getSentiment(keyword):
 				printException(e)
 				return 0
 
+		count += 1
+		progressBar.setValue(count / countMax * 100)
+
 		print()
 	if numSentiments == 0: # Avoid division by zero if there are no files
 		return 0
 	return sumSentiments / numSentiments
 
-def main(keyword):
+# Called on button press
+@Slot()
+def computeBtnClick():
+	global keyword
+	keyword = subjectTxt.text().lower()
+
 	init()
 	sentiment = getSentiment(keyword)
 	print('=' * width)
-	rprint(f'Overall Bitcoin sentiment: {sentiment}')
+	rprint(f'Overall {keyword.capitalize()} sentiment: {sentiment}')
 	if sentiment > 0:
 		rprint('(BUY)')
+		alert(f'The conclusion for "{keyword.capitalize()}" is BUY\n\nSentiment = {sentiment}', 'Sentiment: BUY.')
 	elif sentiment < 0:
 		rprint('(SELL)')
+		alert(f'The conclusion for "{keyword.capitalize()}" is SELL\n\nSentiment = {sentiment}', 'Sentiment: SELL.')
+	else:
+		alert(f'The conclusion for "{keyword.capitalize()}" is WAIT\n\nSentiment = {sentiment}', 'Sentiment: WAIT.')
 
-keyword = input('Enter a sentiment topic: ').lower()
-main(keyword)
+# Make a message dialog
+def alert(msg, title):   
+	ctypes.windll.user32.MessageBoxW(0, msg, title, 0)
+
+# Overrides the current print
+def print(*args, **kwargs):
+	try:
+		if(len(args) > 0):
+			outputTxt.appendPlainText(str(args[0]))
+		else:
+			outputTxt.appendPlainText('')
+		window.update()
+	except:
+		pass
+
+	#__builtin__.print('My overridden print() function!')
+	return __builtin__.print(*args, **kwargs)
+
+global width
+width = 69
+os.system('cls')
+print('=' * width)
+rprint('Starting application...')
+print('=' * width)
+initializeUserInterface()
+#main(keyword)
