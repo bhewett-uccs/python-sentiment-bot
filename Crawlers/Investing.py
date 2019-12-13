2392e909dbe31d18db5548320e20b29b065d232c
Simeon <simeonwuthier@gmail.com>
2019-12-12 19:20:55 -0700

    Update Investing.py

--- Crawlers/Investing.py
+++ Crawlers/Investing.py
@@ -12,22 +12,9 @@ def fetch(keyword):
 	url = f'https://www.investing.com/search/?q={keyword}&tab=news'
 	headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
 	response = requests.get(url, headers=headers, timeout=10)
-	#time.sleep(5)
 	response.raise_for_status()
 	soup = BeautifulSoup(response.text, features='html.parser')
 	
-	# Data has been parsed, now we fetch all the tweet text
-	'''_elements = soup.find_all('div', class_='articleItem')
-	for _element in _elements:
-		elements = _element.find_all('a') + _element.find_all('p')
-		sentimentCounter = 0
-		sentimentSum = 0
-		for element in elements:
-			text = element.get_text()
-			if keyword.lower() in text:
-				print(text)
-				sentimentSum += sentiment(text)
-				sentimentCounter += 1'''
 	sentimentCounter = 0
 	sentimentSum = 0
 	elements = soup.find_all('div', class_='articleItem')
